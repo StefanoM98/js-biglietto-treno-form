5da403fac198ebb0bdb90c624f5d17b4cfc5024b
StefanoM98 <smartino030@gmail.com>
2023-03-09 16:12:32 +0100

    Add: saved name, km and age

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // CREIAMO LA FUNZIONE PER COMPRARE IL BIGLIETTO
 const shopBtn = document.getElementById("shop");
 shopBtn.addEventListener("click", function() {
+    // SALVIAMO IL NOME E COGNOME DELL'UTENTE
+    const nameSurname = document.getElementById("user-name")
+    console.log(nameSurname);
+    // SALVIAMO I KM CHE IL CLIENTE VUOLE FARE
     const kmDistance = document.getElementById("distance");
     console.log(kmDistance);
+    // SALVIAMO L'ETA' DEL CLIENTE
+    const userAge = document.getElementById("user-age")
+    console.log(userAge);
 } )
