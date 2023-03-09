5f4eccb0bfc66bc71664e9973a6d1ff526dff178
StefanoM98 <smartino030@gmail.com>
2023-03-09 16:04:10 +0100

    Add: shop btn and distance

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// CREIAMO LA FUNZIONE PER COMPRARE IL BIGLIETTO
+const shopBtn = document.getElementById("shop");
+shopBtn.addEventListener("click", function() {
+    const kmDistance = document.getElementById("distance");
+    console.log(kmDistance);
+} )
